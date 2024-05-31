c23114d47a63089ccccbdd84bc3cf20cd0cb8362
JeongHunHui <deca3248@gmail.com>
2024-06-01 06:52:07 +0900

    gpt4o, 날짜 필터링

--- fastAPIProject/app.py
+++ fastAPIProject/app.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 from flask_sqlalchemy import SQLAlchemy
 import openai, os
 import json
-from datetime import date
+from datetime import date, datetime
 
 app = Flask(__name__)
 load_dotenv('.env')
@@ -61,7 +61,16 @@ def create_diary():
 
 @app.route('/api/diary', methods=['GET'])
 def get_diaries():
-    diaries = Diary.query.all()
+    date_str = request.args.get('date')
+    if date_str:
+        try:
+            query_date = datetime.strptime(date_str, '%Y%m%d').date()
+            diaries = Diary.query.filter(Diary.created_at == query_date).all()
+        except ValueError:
+            return jsonify({'error': 'Invalid date format. Use YYYYMMDD.'}), 400
+    else:
+        diaries = Diary.query.all()
+    
     return jsonify([diary.to_dict() for diary in diaries])
 
 @app.route('/api/diary/<int:diary_id>', methods=['GET'])
@@ -76,7 +85,7 @@ messages = []
 def get_gpt_response(content):
     messages.append({"role":"user", "content":content})
     completion = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
+        model="gpt-4o",
         messages=messages
     )
     chat_response = completion.choices[0].message.content
@@ -87,16 +96,16 @@ def initialize():
     messages.append({"role":"user", "content":
     '''
 내가 일기를 주면 너는 일기의 감정을 파악해서 아래와 같은 형식으로 json 형태로 음악을 만들어줘.
-{"tracks": [{"notes": [{ "note": 76, "start": 0, "duration": 0.25 }],}, {"notes": [{ "note": [76, 80, 83], "start": 0, "duration": 0.75 ],},],}
-내가 앞으로 뭘 보내던 너는 무조건 위와 같은 json형식으로 줘 다른 말 붙히지마 절대.
-일기를 보내면 너는 json으로만 답해. 위와 같은 형식으로.
-음악의 길이는 8초로 해.
-참고로 node 60은 C, 61은 C#, 62는 B야. 1은 1초야. 참고해서 감정에 따른 음악 만들어줘.
+{"tracks": [{"notes": [{"note": 76,"start": 0,"duration": 0.25},{"note": 76,"start": 0.2,"duration": 0.25}]},{"notes": [{"note": [76,80,83],"start": 0,"duration": 0.75},{"note": [76,81,85],"start": 0.5,"duration": 0.75}]}]}
+note 많이 만들어서 음악의 총 길이가 7초가 되어야한다. start + duration이 7이 되는 note가 있어야한다. note 60은 C, 61은 C#, 62는 B야. start와 duration에서 1은 1초야.
+일기를 보내면 너는 무조건 절대적으로 json으로만 답해. 위와 같은 형식으로. 다른 말 붙히지마 절대.
+참고로  참고해서 감정에 따른 음악 만들어줘.
 감정이 우울하다고 판단하면 단조, 기쁘면 단조 이렇게.
-화음, 멜로디를 나눠서. 악기는 피아노야.
+화음, 멜로디를 나눠서. 악기는 피아노야. 박자도 다양하게 해줘. 신나는 분위기는 짧은 note, 우울하면 긴 note, 그 밖에는 알아서 잘 해줘.
+```json ``` 이런식으로 코드 블럭에 넣지 마라.
     '''})
     completion = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
+        model="gpt-4o",
         messages=messages
     )
     chat_response = completion.choices[0].message.content
