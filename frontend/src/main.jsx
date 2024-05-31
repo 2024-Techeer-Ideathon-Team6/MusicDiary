12ade23efb6fe0168c0cd9dd7b350f952b09d61b
jiwon <oneknow20@gmail.com>
2024-06-01 05:49:39 +0900

    캘린더랑 목록 퍼블리싱

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
+import Testtemp from './components/Testtemp.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <Testtemp/>
+  </React.StrictMode>
 );
